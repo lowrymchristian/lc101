95b7147b9f8113ccbaa04fbfdeaa2351aa79e515
lowrymchristian <lowrymchristian@gmail.com>
2022-06-04 09:33:23 -0500

    objects

---
+++ objectAndMathObjectPractice.js
@@ -0,0 +1,55 @@
+// --- Object practice -----
+
+// let exampleObject = {
+//   'animalType': 'elephant', // this is a property
+//   'petPeeve': 'loud eaters',
+//   'introduceMyself': function(){ // this is a method
+    
+//     let outputString = `I'm an ${this.animalType} and I hate ${this.petPeeve}`;
+//     return outputString;
+    
+//   }
+// }
+
+// console.log(typeof exampleObject);
+
+// let introduction = exampleObject.introduceMyself();
+// console.log(introduction);
+
+// let whatAnimalIsIt = exampleObject.animalType; // this is dot notation
+// console.log(whatAnimalIsIt);
+
+// let beeInTheBonnet = exampleObject['petPeeve']; // this is bracket notation
+// console.log(beeInTheBonnet);
+
+// console.log(outputString);
+
+
+
+//---------------------
+
+// ----- Math object practice ------
+
+// let floorExample1 = 100.99;
+// console.log(Math.floor(floorExample1));
+// let floorExample2 = 100.01;
+// console.log(Math.floor(floorExample2));
+// let floorExample3 = 100.99;
+// console.log(Math.floor(floorExample3));
+
+// let ceilingExample1 = 100.99;
+// console.log(Math.ceil(ceilingExample1));
+// let ceilingExample2 = 100.01;
+// console.log(Math.ceil(ceilingExample2));
+// let ceilingExample3 = 100.00;
+// console.log(Math.ceil(ceilingExample3));
+
+// let randNum = Math.random();
+// console.log(randNum);
+// let flooredRandomNumber = Math.floor(randNum);
+// console.log(flooredRandomNumber);
+// let roundedRandomNumber = Math.round(randNum);
+// console.log(roundedRandomNumber);
+
+//---------------------
+
